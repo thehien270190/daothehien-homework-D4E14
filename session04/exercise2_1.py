1278ba579d950d70a3b1280299f922d150ffbe96
thehien270190 <daothehien27190@gmail.com>
2020-07-27 03:10:13 +0700

    initial commit

--- session04/exercise2_1.py
+++ session04/exercise2_1.py
@@ -3,6 +3,8 @@ inventory = {
     'pouch' : ['flint', 'twine', 'gemstone'],
     'backpack' : ['xylophone', 'dagger', 'bedroll', 'bread loaf']
             }
+print(inventory)
 inventory['pocket'] = ['seashell', 'strange berry', 'lint']
 inventory['backpack'].remove('dagger')
 inventory['gold'] = inventory['gold'] + 50
+print(inventory)
